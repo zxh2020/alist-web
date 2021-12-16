daba5cbab8030c5af1a3fc996ae4fc576eef8da7
Xhofe <i@nn.ci>
2021-12-16 15:57:27 +0000

    upload 223366a assets files

---
+++ v2/223366a/assets/useTitle.391ac300.js
@@ -0,0 +1,1 @@
+import{r as t}from"./vendor.ef86dff2.js";var e=(e,o=!1)=>{t.exports.useEffect((()=>{const t=document.title;return document.title=o?`${e} - alist`:e,()=>{document.title=t}}),[])};export{e as u};
