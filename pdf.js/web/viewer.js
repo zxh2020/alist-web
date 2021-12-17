f029a3c0240651ba82926c8c0897dc41c03dcef3
Xhofe <i@nn.ci>
2021-12-17 15:29:04 +0800

    Update viewer.js

--- pdf.js/web/viewer.js
+++ pdf.js/web/viewer.js
@@ -2168,7 +2168,7 @@ const PDFViewerApplication = {
 exports.PDFViewerApplication = PDFViewerApplication;
 let validateFileURL;
 {
-  const HOSTED_VIEWER_ORIGINS = ["null", "http://mozilla.github.io", "https://mozilla.github.io"];
+  const HOSTED_VIEWER_ORIGINS = ["null", "http://xhofe.github.io", "https://xhofe.github.io"];
 
   validateFileURL = function (file) {
     if (file === undefined) {
