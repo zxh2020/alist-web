32589c6f2de1417404d4897579baa9e801de5c4d
Xhofe <i@nn.ci>
2021-12-21 14:07:34 +0000

    upload 2805ed1 assets files

---
+++ v2/2805ed1/assets/useTitle.500cc51b.js
@@ -0,0 +1,1 @@
+import{r as t}from"./vendor.1fd8fd08.js";var e=(e,o=!1)=>{t.exports.useEffect((()=>{const t=document.title;return document.title=o?`${e} - alist`:e,()=>{document.title=t}}),[])};export{e as u};
