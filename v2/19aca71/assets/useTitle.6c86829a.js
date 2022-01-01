516c93cce5017a194946dab083832633353ee897
Xhofe <i@nn.ci>
2022-01-01 13:42:36 +0000

    upload 19aca71 assets files

---
+++ v2/19aca71/assets/useTitle.6c86829a.js
@@ -0,0 +1,1 @@
+import{r as t}from"./vendor.630a5f1f.js";var e=(e,o=!1)=>{t.exports.useEffect((()=>{const t=document.title;return document.title=o?`${e} - alist`:e,()=>{document.title=t}}),[])};export{e as u};
