d3ade96d115dc1d64135458599a88f7859b093b4
Xhofe <i@nn.ci>
2022-01-02 07:20:45 +0000

    upload d045b12 assets files

---
+++ v2/d045b12/assets/useTitle.c616e87e.js
@@ -0,0 +1,1 @@
+import{r as t}from"./vendor.a4c78c95.js";var e=(e,o=!1)=>{t.exports.useEffect((()=>{const t=document.title;return document.title=o?`${e} - alist`:e,()=>{document.title=t}}),[])};export{e as u};
